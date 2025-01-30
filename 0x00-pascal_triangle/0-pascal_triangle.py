1df6c41d9f09d6e79b23ce4335c3f22b14e32cac
sahle-1996 <sahlesiumgoitom@gmail.com>
2025-01-30 18:42:40 +0000

    udpate

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -13,11 +13,12 @@ def pascal_triangle(n):
     """
     if n <= 0:
         return []
-    triangle = [[1]]
-    for i in range(1, n):
+    triangle = []
+    for i in range(n):
         row = [1]
         for j in range(1, i):
             row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
-        row.append(1)
+        if i > 0:
+            row.append(1)
         triangle.append(row)
     return triangle
